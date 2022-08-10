41f5dd0593351f6303cdaec72f77ea82d43798aa
ItVal <valentinhonvj1@gmail.com>
2022-08-10 19:25:08 +0200

    feat : create function main

---
+++ src/Components/Main.js
@@ -0,0 +1,40 @@
+
+function Main() {
+    return (
+      <div className="profil">
+        <div className="profil_image">
+          <img className="image_item" src='' alt="logoReact" />
+        </div>
+  
+        <div className="profile_name">
+          <h1>Valentin Nasibu </h1>
+          <h4>Frontend Developper</h4>
+          <p>irnasvalentin@gmail.com</p>
+          <button>
+          <img src="" alt="logoReact"  /> Email
+          </button>
+        </div>
+  
+        <div className="profile_contents">
+          <h2>About</h2>
+          <p>
+            I am a frontend developer with a particular interest in making things
+            simple and automating daily tasks. I try to keep up with security and
+            best practices, and am always looking for new things to learn.
+          </p>
+          <h2>Interests</h2>
+          <p>
+            Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+            Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+          </p>
+        </div>
+  
+        <div className="profile_foot">
+          <ul>
+            
+          </ul>
+        </div>
+      </div>
+    );
+  }
+  export default Main;
